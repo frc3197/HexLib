2b5384ee02e16aeb573cb93a4f0306b3375f86a5
Yuri <kleymanyy@gmail.com>
2021-10-26 14:41:58 -0500

    Create SwerveAutoParameters.java

---
+++ HexLib/src/main/java/frc/robot/HexLib/DriveTrain/SwerveDrive/SwerveDriveUtil/SwerveAutoParameters.java
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.HexLib.DriveTrain.SwerveDrive.SwerveDriveUtil;
+
+/** Add your docs here. */
+public class SwerveAutoParameters {
+    /*
+    - Holo X Y PID
+    - Holo Rot ProfPid
+    - stateStdDevs, localMeasurementStdDevs, visionMeasurementStdDevs
+    - initalPos
+
+
+    */
+}
